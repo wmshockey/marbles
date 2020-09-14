84f42efe8b700d04e20ed0abd7dbf65a98915bb6
Warren Shockey <wmshockey@gmail.com>
2020-09-14 09:43:21 -0600

    successfully added devise user authentication

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_14_141508) do
+ActiveRecord::Schema.define(version: 2020_09_14_151938) do
 
   create_table "games", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.string "name"
@@ -34,6 +34,18 @@ ActiveRecord::Schema.define(version: 2020_09_14_141508) do
     t.string "bplayer"
     t.string "gbteam"
     t.string "ryteam"
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
